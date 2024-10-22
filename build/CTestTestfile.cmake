89c5c58c6de622a67a3b4e599d584f3b7a0510a9
2OOIQ-Boy <thibou.05@gmail.com>
2024-10-22 12:51:02 +0200

    esperant que sa marche

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ytyanndavid/Documents/prog-c/Puzzle_en_C
-# Build directory: /Users/ytyanndavid/Documents/prog-c/Puzzle_en_C/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_tyao_dummy "./game_test_tyao" "dummy")
-set_tests_properties(test_tyao_dummy PROPERTIES  _BACKTRACE_TRIPLES "/Users/ytyanndavid/Documents/prog-c/Puzzle_en_C/CMakeLists.txt;18;add_test;/Users/ytyanndavid/Documents/prog-c/Puzzle_en_C/CMakeLists.txt;0;")
